06c4e305bfcadab99fb4ec26356185b75bd331c8
laurensvanheems <laurensvanheems@gmail.com>
2019-05-05 17:22:11 -0400

    Adds events

--- src/js/cms.js
+++ src/js/cms.js
@@ -2,7 +2,7 @@ import React from "react";
 import CMS from "netlify-cms";
 
 import HomePreview from "./cms-preview-templates/home";
-import PostPreview from "./cms-preview-templates/post";
+import EventPreview from "./cms-preview-templates/event";
 import ProductsPreview from "./cms-preview-templates/products";
 import ValuesPreview from "./cms-preview-templates/values";
 import ContactPreview from "./cms-preview-templates/contact";
@@ -22,7 +22,7 @@ class ColorControl extends React.Component {
 
 CMS.registerPreviewStyle("/css/main.css");
 CMS.registerPreviewTemplate("home", HomePreview);
-CMS.registerPreviewTemplate("post", PostPreview);
+CMS.registerPreviewTemplate("event", EventPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
